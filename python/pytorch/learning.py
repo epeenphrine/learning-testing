799a83555f23be16a906b765f76345cc2ece0786
dong <donghyunwon610@gmail.com>
2020-05-13 15:03:34 -0400

    sync

--- python/pytorch/learning.py
+++ python/pytorch/learning.py
@@ -107,4 +107,118 @@ X = X.view(-1,28*28)
 output= net(X)
 
 print(output)
-# %% training neural network  
+# %% training neurai networks 
+import torch
+import torchvision
+from torchvision import transforms, datasets
+import torch.nn as nn
+import torch.nn.functional as F
+
+train = datasets.MNIST('', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ]))
+
+test = datasets.MNIST('', train=False, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ]))
+
+
+trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
+testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=False)
+REBUILD_DATA = True
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(28*28, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1)
+
+net = Net()
+print(net)
+
+import torch.optim as optim
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+
+for epoch in range(3): # 3 full passes over the data
+    for data in trainset:  # `data` is a batch of data
+        X, y = data  # X is the batch of features, y is the batch of targets.
+        net.zero_grad()  # sets gradients to 0 before loss calc. You will do this likely every step.
+        output = net(X.view(-1,784))  # pass in the reshaped batch (recall they are 28x28 atm)
+        loss = F.nll_loss(output, y)  # calc and grab the loss value
+        loss.backward()  # apply this loss backwards thru the network's parameters
+        optimizer.step()  # attempt to optimize weights to account for loss/gradients
+    print(loss)  # print loss. We hope
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in testset:
+        X, y = data
+        output = net(X.view(-1,784))
+        #print(output)
+        for idx, i in enumerate(output):
+            #print(torch.argmax(i), y[idx])
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+
+print("Accuracy: ", round(correct/total, 3))
+
+#%% convol nn
+
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+REBUILD_DATA = True
+
+class DogVsCats():
+    IMG_SIZE = 50
+    CATS =  "PetImages/Cat"
+    DOGS = "PetImages/Dog"
+    Testing = "PetImages/training"
+    LABELS = {CATS: 0, DOGS: 1}
+    training_data = [] 
+
+    catcount = 0
+    dogcount = 0 
+
+    def make_training_data(self):
+        for label in self.LABELS:
+            for f in tqdm(os.listdir(label)):
+                try:
+                    if "jpg" in f:
+                        path = os.path.join(label,f)
+                        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                        img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
+                        self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])
+                        if label == self.CATS:
+                            self.catcount += 1
+                        elif label == self.DOGS:
+                            self.dogcount += 1
+                except Exception as e:
+                    pass
+        np.random.shuffle(self.training_data)
+        np.save("training_Data.npy", self.training_data)
+
+if REBUILD_DATA:
+    dogsvscats = DogVsCats()
+    dogsvscats.make_training_data()
+    print("CATS:", dogsvscats.catcount)
+    print("DOGS: ", dogsvscats.dogcount)
